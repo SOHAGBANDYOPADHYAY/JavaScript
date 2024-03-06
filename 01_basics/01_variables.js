3abfe552262bbfffcb826cf538b6f6ce7ac9b19e
SOHAGBANDYOPADHYAY <bandyopadhyaysohag5@gmail.com>
2024-03-06 11:12:34 +0000

    files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId= 19934
+let accountEmail = "sohag@goole,com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"Use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browser.
+
+let name = "sohag" //string dataType
+let age = 24 // number dataType
+let IsLoggedIn = true // boolean
+
+
+// number => 2 to the power 53
+// bigint => to handle big data number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined i.g let accountState;
+// symbol => unique
+
+// object 
+// typeof => it will tell the dataType
