71671a22f7376b85a36b823b5aa26d3859001732
SOHAGBANDYOPADHYAY <bandyopadhyaysohag5@gmail.com>
2024-03-13 05:50:48 +0000

    files added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="55"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "55"=> 55
+// "55abc" => NaN
+// true => 1; false => 0
+
+
+//    ******************************Operations**************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " sohag"
+console.log( str1 + str2);
+
+// in conversion if string is in first position then whole will convert in string. This comment is just for referrence like this code 
+//console.log( "1" +2 +1); output will be 121 as string
