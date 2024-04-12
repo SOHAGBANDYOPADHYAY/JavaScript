703787ba45450fbf1a3c59a418e9f164fe09a9b6
SOHAGBANDYOPADHYAY <bandyopadhyaysohag5@gmail.com>
2024-04-13 01:14:41 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("sohag"))
+console.log(Array.from("sohag"))
+console.log(Array.from({name: "sohag"})) // interesting
 
 let score1 = 100
 let score2 = 200
