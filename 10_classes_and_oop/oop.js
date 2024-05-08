1102fcf23303025a89d4f8cac034c1c34e28b7e8
SOHAGBANDYOPADHYAY <bandyopadhyaysohag5@gmail.com>
2024-05-08 18:01:55 +0000

    files added

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -15,11 +15,11 @@ const user = {
 
 //console.log(user.username)
 //console.log(user.getUserDetails());
-// console.log(this);
+// console.log(this); in browser it will give "window"
 
 
 function User(username, loginCount, isLoggedIn){
-    this.username = username;
+    this.username = username;                  //here this.username is variable
     this.loginCount = loginCount;
     this.isLoggedIn = isLoggedIn
 
@@ -35,3 +35,8 @@ const userOne = new User("hitesh", 12, true)
 const userTwo = new User("ChaiAurCode", 11, false)
 console.log(userOne.constructor);
 //console.log(userTwo);
+
+// when we start to use new keyword first an empty object will be created which is called instances.
+// Then a constructor function is called because of new keyword and packed all argument and returns to user.
+// Thirdly "this keyword" then injected in all arguments.
+// Fourthly we got all these in function.
