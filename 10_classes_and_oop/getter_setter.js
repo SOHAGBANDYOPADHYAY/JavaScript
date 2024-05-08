74e6d688cfa37721388682a94bab5bad32ce79c4
SOHAGBANDYOPADHYAY <bandyopadhyaysohag5@gmail.com>
2024-05-08 21:02:54 +0000

    files added

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -4,14 +4,14 @@ class User {
         this.password = password
     }
 
-    get email(){
+    get email(){         
         return this._email.toUpperCase()
     }
     set email(value){
         this._email = value
     }
 
-    get password(){
+    get password(){                                        //here we will get the value of password written on line no 4
         return `${this._password}hitesh`
     }
 
